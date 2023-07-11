1964a551e34e8678d106008a6e8b557239a5c633
Mehak Basrani <mehakbasrani17@gmail.com>
2023-07-12 00:11:48 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://mehakbasrani17:Mehak@cluster0.whdmn8s.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Connected to mongoDB..");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(port, hostname, () => {
+  connect();
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,45 @@
+import useFetch from "../../hooks/useFetch";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+
+  const { data, loading, error } = useFetch(
+    "http://127.0.0.1:8000/api/hotels?featured=true"  
+   );
+  
+    const images = [
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdynamic-media-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F12%2F5e%2Fb4%2Fa7%2Fhotel-facade.jpg%3Fw%3D700%26h%3D-1%26s%3D1&tbnid=nvmK7tdBHVDfBM&vet=12ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ..i&imgrefurl=https%3A%2F%2Fwww.tripadvisor.in%2FHotel_Review-g494941-d13431027-Reviews-WOW_Hotel-Indore_Indore_District_Madhya_Pradesh.html&docid=B95zLQecWV8rMM&w=700&h=371&q=hotel%20wow%20indore&ved=2ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdynamic-media-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F12%2F5e%2Fb4%2Fa7%2Fhotel-facade.jpg%3Fw%3D700%26h%3D-1%26s%3D1&tbnid=nvmK7tdBHVDfBM&vet=12ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ..i&imgrefurl=https%3A%2F%2Fwww.tripadvisor.in%2FHotel_Review-g494941-d13431027-Reviews-WOW_Hotel-Indore_Indore_District_Madhya_Pradesh.html&docid=B95zLQecWV8rMM&w=700&h=371&q=hotel%20wow%20indore&ved=2ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdynamic-media-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F12%2F5e%2Fb4%2Fa7%2Fhotel-facade.jpg%3Fw%3D700%26h%3D-1%26s%3D1&tbnid=nvmK7tdBHVDfBM&vet=12ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ..i&imgrefurl=https%3A%2F%2Fwww.tripadvisor.in%2FHotel_Review-g494941-d13431027-Reviews-WOW_Hotel-Indore_Indore_District_Madhya_Pradesh.html&docid=B95zLQecWV8rMM&w=700&h=371&q=hotel%20wow%20indore&ved=2ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdynamic-media-cdn.tripadvisor.com%2Fmedia%2Fphoto-o%2F12%2F5e%2Fb4%2Fa7%2Fhotel-facade.jpg%3Fw%3D700%26h%3D-1%26s%3D1&tbnid=nvmK7tdBHVDfBM&vet=12ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ..i&imgrefurl=https%3A%2F%2Fwww.tripadvisor.in%2FHotel_Review-g494941-d13431027-Reviews-WOW_Hotel-Indore_Indore_District_Madhya_Pradesh.html&docid=B95zLQecWV8rMM&w=700&h=371&q=hotel%20wow%20indore&ved=2ahUKEwjZ0paxhPz_AhUpzKACHUkeDVYQMygDegUIARDXAQ"
+    ]
+   
+  return (
+    <div className="fp">
+      {loading ? 
+      ("Loading Please Wait!"
+      ) : (
+      <>
+     { data.map((item) =>(
+        <div className="fpItem" key={item._id}>
+         <img
+          //src={item.photos[0]}
+          src = {images}
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">{item.name}</span>
+        <span className="fpCity">{item.city}</span>
+        <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+        {item.rating && <div className="fpRating">
+          <button>{item.rating}</button>
+          <span>Excellent</span>
+        </div>}
+      </div>))}
+      
+      </>)}
+    </div>
+  );
+};
+
+export default FeaturedProperties;
